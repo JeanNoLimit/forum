b2446e03d6b4dbd169b7be2a0ab38aacc62cc473
Jean-Noël Richert <jean.noel.richert@gmail.com>
2023-04-28 15:27:43 +0200

    suppriession d'une requête inutile.

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -3,7 +3,7 @@
     
     use App\Manager;
     use App\DAO;
-    use Model\Managers\TopicManager;
+    // use Model\Managers\TopicManager;
 
     class TopicManager extends Manager{
 
@@ -15,19 +15,7 @@
             parent::connect();
         }
 
-        // Requête pour trouver tous les posts d'un topic
-        public function findPostByTopic($id) {
-            
-            $sql = "SELECT *
-                    FROM ".$this->tableName." p
-                    WHERE p.topic_id= :id";
-                    //La requête renvoie plusieurs objets, d'où getMultipleResults
-            return $this->getMultipleResults(
-                DAO::select($sql, ['id'=> $id]),
-                $this->className
-            );  
-        }
-        
+
         //Requête pour afficher les topics d'une catégorie?
         public function findTopicsByCategory($id){
 
