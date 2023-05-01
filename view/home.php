b340574bdae2dd5aa30f16a571575b9c4d5a1176
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-01 15:47:06 +0200

    Mise en page Layout + page d'accueil

--- view/home.php
+++ view/home.php
@@ -1,9 +1,14 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<div id="accueil">
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+    <h1>BIENVENUE SUR LE FORUM</h1>
 
-<p>
-    <a href="/security/login.html">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="/security/register.html">S'inscrire</a>
-</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+    <p>Pour accéder au contenu du forum veuillez vous connecter ou vous inscrire </p>
+
+    <p>
+        <a href="/security/login.html">Se connecter</a>
+        <span>&nbsp;-&nbsp;</span>
+        <a href="/security/register.html">S'inscrire</a>
+    </p>
+
+</div>
--- view/layout.php
+++ view/layout.php
@@ -42,8 +42,8 @@
                             ?>
                             <a href="./view/security/login.php">Connexion</a>
                             <a href="/security/register.html">Inscription</a>
-                            <a href="index.php?ctrl=forum&action=listTopics">la liste des topics</a>
-                            <a href="index.php?ctrl=forum&action=listCategory">la liste des categories</a>
+                            <a href="index.php?ctrl=forum&action=listTopics">Liste des topics</a>
+                            <a href="index.php?ctrl=forum&action=listCategory">Liste des categories</a>
                         <?php
                         }
                    
