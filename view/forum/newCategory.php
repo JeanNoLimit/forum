4e6135e329bf6164ef3c54ea1c5af6c6292ec269
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-02 14:06:51 +0200

    Création du formulaire newCategory

--- view/forum/newCategory.php
+++ view/forum/newCategory.php
@@ -3,3 +3,20 @@
 <div id="title">
     <h1>Nouvelle catégorie</h1>
 </div>
+
+
+<div id="borderPost">
+
+   <form action="index.php?ctrl=forum&action=newCategory" method="post">
+
+        <div>
+            <label for="titleCategory"> Titre du sujet 
+                <input type="text" name="titleCategory" id="titleCategory">
+            </label>
+        </div>
+
+        <div>
+            <input type="submit" value="Envoyer" name="submitNewCategory">
+        </div>
+    </form>
+</div>
