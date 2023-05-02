d8c81f3ce0c26503a103c919bbd2694f80eec5b8
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-02 09:01:55 +0200

    Insertion du formulaire nouveau Post

--- view/forum/listPost.php
+++ view/forum/listPost.php
@@ -6,14 +6,14 @@ $topic = $result["data"]["topic"];
 // $topicName=$topic->getTitle();
 // echo $topicName;
 ?>
-
+                                        <!-- Affichage des posts -->
 <div id="postTitle">
     <h1><?=$topic->getTitle()?></h1>
     <p class="creationDate">date de création : le <?=$topic->getCreationDate()->format("d-m-Y")?></p>
 </div>
 <div id="borderPost">
 <div class="button newPost">
-        <a href="">Nouveau message</a>
+        <a href="#newMessage">Nouveau message</a>
     </div>
 
 <?php
@@ -33,6 +33,20 @@ foreach($posts as $post){ ?>
     </div>
 
 <?php } ?>
+</div>
+                                        <!-- Formulaire de saisie d'un nouveau post -->
+
+<div id="postTitle">
+    <h3>Nouveau message</h3>
+</div>
+<div id="borderPost">
+
+    <form action="index.php?ctrl=forum&action=listPosts" method="post">
+        <div id="newMessage">
+            <label for="message">message</label>
+                <textarea name="message" id="message" cols="80" rows="10"></textarea>
+        </div>
+        <input type="submit" value="Envoyer">
+    </form>
 
 </div>
-
