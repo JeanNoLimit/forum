fe970e8015cab5b21817e3aebaf5a64fc686589d
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-04 16:58:15 +0200

    Requêtes : recherche pseudo et mdp dans la bdd

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -19,6 +19,28 @@ class UserManager extends Manager{
 
     }
 
+    //Requête pour vérifie l'existence de l'adresse mail dans la bdd
+    public function findEmail($email){
+
+        $sql="SELECT *
+            FROM ".$this->tableName."
+            WHERE email LIKE :email";
+
+        return $this->getOneOrNullResult(
+            DAO::select($sql,['email' => $email], false), $this->className
+        );
+    }
+    //Requête pour vérifier l'existence de pseudo dans la bdd
+    public function findPseudo($pseudo){
+        $sql="SELECT *
+            FROM ".$this->tableName."
+            WHERE pseudo LIKE :pseudo";
+
+        return $this->getOneOrNullResult(
+            DAO::select($sql,['pseudo' => $pseudo],false), $this->className
+        );
+        
+    }
 
 
 
