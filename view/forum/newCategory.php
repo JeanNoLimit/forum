b25143df580c46073f7a8de1c09a289ed2f8ce5c
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-07 17:40:41 +0200

    Maj CSS formulaire nouvelle catégorie

--- view/forum/newCategory.php
+++ view/forum/newCategory.php
@@ -1,22 +1,22 @@
 
+<div class="formContainerCat">
+    <div class="title">
+        <h1>Nouvelle catégorie</h1>
+    </div>
 
-<div class="title">
-    <h1>Nouvelle catégorie</h1>
-</div>
 
+    <div id="borderContainer">
 
-<div id="borderContainer">
+    <form action="index.php?ctrl=forum&action=newCategory" method="post" class="formTopicAndCat">
 
-   <form action="index.php?ctrl=forum&action=newCategory" method="post">
+            <div>
+                <label for="categoryName"> Titre de la nouvelle catégorie 
+                    <input type="text" name="categoryName" id="categoryName" class="inputField newTitle">
+                </label>
+            </div>
 
-        <div>
-            <label for="categoryName"> Titre du sujet 
-                <input type="text" name="categoryName" id="categoryName">
-            </label>
-        </div>
-
-        <div>
-            <input type="submit" value="Envoyer" name="submitNewCategory">
-        </div>
-    </form>
+                <input type="submit" value="Envoyer" name="submitNewCategory" class="submitButton">
+            
+        </form>
+    </div>
 </div>
--- view/forum/newTopic.php
+++ view/forum/newTopic.php
@@ -3,45 +3,46 @@ $categories = $result["data"]["categories"];
 
 ?>
 
-
-<div class="title">
-    <h1>Nouveau sujet</h1>
-</div>
-
-<div id="borderContainer">
-
-   <form action="index.php?ctrl=forum&action=newTopic" method="post" class="formTopic ">
-
-    <div>
-        <label for="titleTopic"> Titre du sujet 
-            <input type="text" name="titleTopic" id="titleTopic" class="inputField newTitle">
-        </label>
+<div class="formContainerTopic">
+    <div class="title">
+        <h1>Nouveau sujet</h1>
     </div>
 
-    <div>
-        <label for="category"> Catégorie
-            <select name="category_id" id="category" class="inputField">
-                <?php foreach($categories as $category) { ?>
-                    <option value="<?= $category->getId(); ?>"><?= $category->getCategoryName();?></option>
-                <?php } ?>    
-            </select>
-        </label>
-        <p class="newCatLink">Si la catégorie n'est pas présente dans la liste, veuillez <a href="index.php?ctrl=forum&action=newCategory">cliquer ici </a></p>
+    <div id="borderContainer">
+
+    <form action="index.php?ctrl=forum&action=newTopic" method="post" class="formTopicAndCat ">
+
+        <div>
+            <label for="titleTopic"> Titre du sujet 
+                <input type="text" name="titleTopic" id="titleTopic" class="inputField newTitle">
+            </label>
+        </div>
+
+        <div>
+            <label for="category"> Catégorie
+                <select name="category_id" id="category" class="inputField">
+                    <?php foreach($categories as $category) { ?>
+                        <option value="<?= $category->getId(); ?>"><?= $category->getCategoryName();?></option>
+                    <?php } ?>    
+                </select>
+            </label>
+            <p class="newCatLink">Si la catégorie n'est pas présente dans la liste, veuillez <a href="index.php?ctrl=forum&action=newCategory">cliquer ici </a></p>
+        </div>
+                        
+        <div>
+            <label for="message">Message  </label>
+                <textarea name="message" id="message" cols="80" rows="10" class=TopicTextArea></textarea>
+        
+        </div>
+
+
+            <input type="submit" value="Envoyer" name="submitNewTopic" class="submitButton">
+
+        
+    </form> 
+
+
+
+
     </div>
-                    
-    <div>
-        <label for="message">Message  </label>
-            <textarea name="message" id="message" cols="80" rows="10" class=TopicTextArea></textarea>
-       
-    </div>
-
-
-        <input type="submit" value="Envoyer" name="submitNewTopic" class="submitButton">
-
-    
-   </form> 
-
-
-
-
 </div>
