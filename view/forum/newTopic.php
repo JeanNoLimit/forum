bcb057f5c44234ab1167c055bbae6fc372e4c499
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-07 17:20:57 +0200

    MAJ CSS -Formulaire nouveau topic

--- view/forum/newTopic.php
+++ view/forum/newTopic.php
@@ -10,39 +10,35 @@ $categories = $result["data"]["categories"];
 
 <div id="borderContainer">
 
-   <form action="index.php?ctrl=forum&action=newTopic" method="post">
+   <form action="index.php?ctrl=forum&action=newTopic" method="post" class="formTopic ">
 
     <div>
         <label for="titleTopic"> Titre du sujet 
-            <input type="text" name="titleTopic" id="titleTopic">
+            <input type="text" name="titleTopic" id="titleTopic" class="inputField newTitle">
         </label>
     </div>
 
     <div>
         <label for="category"> Catégorie
-            <select name="category_id" id="category">
+            <select name="category_id" id="category" class="inputField">
                 <?php foreach($categories as $category) { ?>
                     <option value="<?= $category->getId(); ?>"><?= $category->getCategoryName();?></option>
                 <?php } ?>    
             </select>
         </label>
+        <p class="newCatLink">Si la catégorie n'est pas présente dans la liste, veuillez <a href="index.php?ctrl=forum&action=newCategory">cliquer ici </a></p>
     </div>
-                    <p>Si la catégorie n'est pas présente dans la liste, veuillez <a href="index.php?ctrl=forum&action=newCategory">cliquer ici </a></p>
+                    
     <div>
-        <label for="message">Message 
-            <textarea name="message" id="message" cols="80" rows="10"></textarea>
-        </label>
+        <label for="message">Message  </label>
+            <textarea name="message" id="message" cols="80" rows="10" class=TopicTextArea></textarea>
+       
     </div>
 
-    <div>
-        <input type="submit" value="Envoyer" name="submitNewTopic">
-    </div>
+
+        <input type="submit" value="Envoyer" name="submitNewTopic" class="submitButton">
+
     
-
-
-
-
-
    </form> 
 
 
