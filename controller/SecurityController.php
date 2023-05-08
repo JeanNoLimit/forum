c9a64f5d1e738682fbbb7469d5097780d44a738a
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-08 21:57:06 +0200

    MAJ register, messages erreurs saisies

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -42,31 +42,46 @@ class SecurityController extends AbstractController implements ControllerInterfa
         
             if($pseudo && $email && $password && $password2){
                 
-                //On vérifie l'existance du pseudo et de l'adresse mail dans la bdd
-                
-                if($userManager->findEmail($email)){
-                    Session::addFlash("error", "adresse mail déja utiliséé");
+                if(!$userManager->findEmail($email)){
+                    if(strlen($email)>=10 && strlen($email)<=50){
+                        if(!$userManager->findpseudo($pseudo)){
+                            if(strlen($pseudo)>=4 && strlen($pseudo)<=20){
+                                //rappel : strcmp -> comparaison binaire de chaînes. La comparaison est sensible à la casse.
+                                if(strcmp($password, $password2) ==0){
+                                    if(strlen($password)>=4 && strlen($password)<=16){
+                                         // Si l'inscription c'est bien passé :
+                                        $password=password_hash($password,PASSWORD_DEFAULT);
+                                        //Rôle par défaut -> USER
+                                        $data=["email" => $email, "pseudo" => $pseudo, "password" => $password, "role" => "USER"];
+                                        $userManager->add($data);
+                                       
+                                        Session::addFlash("success", "Incription réussie!");
+                                        $this-> redirectTo("security","signin");
+                                    }else{
+                                        Session::addFlash("error", "Le mot de passe doit contenir entre 4 et 16 caractères");
+                                        $this->redirectTo("security","signup");
+                                    }
+                                }else{
+                                    Session::addFlash("error", "Les mots de passe ne correspondent pas");
+                                    $this->redirectTo("security","signup");
+                                }
+                            }else{
+                                Session::addFlash("error", "Le pseudo doit contenir entre 4 et 20 caractères");
+                                $this->redirectTo("security","signup");
+                            }
+                        }else{
+                            Session::addFlash("error", "Pseudo déja utilisé. Veuillez entrer un pseudo valide");
+                            $this->redirectTo("security","signup");
+                        }
+                    }else{
+                        Session::addFlash("error", "Veuillez entrer une adresse valide");
+                        $this->redirectTo("security","signup");
+                    }
+                }else{
+                    Session::addFlash("error", "Adresse mail déja utilisée");
                     $this->redirectTo("security","signup");
-                }elseif($pseudoBdd=$userManager->findPseudo($pseudo)){
-                    Session::addFlash("error", "Pseudo déja utilisé, veuillez entrer un pseudo valide");
-                    $this->redirectTo("security","signup");
-                    //rappel : strcmp -> comparaison binaire de chaînes. La comparaison est sensible à la casse.
-                }elseif(strcmp($password, $password2) !==0){
-                    Session::addFlash("error", "Les mots de passe ne correspondent pas");
-                    $this->redirectTo("security","signup");
-                }else {
-                    $password=password_hash($password,PASSWORD_DEFAULT);
-                    //Rôle par défaut -> USER
-                    $data=["email" => $email, "pseudo" => $pseudo, "password" => $password, "role" => "USER"];
-                  
-                    $userManager->add($data);
-
-                    Session::addFlash("success", "Incription réussie!");
-
-                     // Si l'inscription c'est bien passé :
-                    $this-> redirectTo("security","signin");
-
                 }
+              
 
             }
         }
